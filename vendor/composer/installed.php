9d40f671acec4826f8c0ac48cd3824c98e735f32
riku <yama_moto_g_osaka@icloud.com>
2022-01-16 19:28:16 +0900

    add composer.json, composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9671df96c2182051eb5679e609d9a54586693f7b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9671df96c2182051eb5679e609d9a54586693f7b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
